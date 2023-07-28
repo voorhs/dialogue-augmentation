8e1e4c73a9a8286f142c268e58aeb8eff4b1676c
voorhs <ilya_alekseev_2016@list.ru>
2023-07-29 01:38:17 +0300

    implemented base augs and similarity functions

--- textattack-augment.sh
+++
@@ -1,8 +0,0 @@
-textattack augment\
-    --input-csv utterances.csv\
-    --output-csv augmented.csv\
-    --input-column text\
-    --recipe embedding\
-    --pct-words-to-swap .1\
-    --transformations-per-example 1\
-    --exclude-original
