ef79533a7c525673ce61d64a060582995b5c6f3c
voorhs <ilya_alekseev_2016@list.ru>
2024-02-15 15:08:11 +0300

    added choice of pooling for embedding

--- mylib/modeling/dialogue/baseline_dialogue_encoder.py
+++ mylib/modeling/dialogue/baseline_dialogue_encoder.py
@@ -1,17 +1,36 @@
+from dataclasses import dataclass
+
 from torch import nn
 from transformers import AutoModel, AutoTokenizer
+
 from ...utils.training import HParamsPuller
+from ...utils.modeling import CLSPooling, AveragePooling, SelfAttentionPooling
 from .base_dialogue_model import BaseDialogueModel
 
 
+@dataclass
+class BaselineDialogueEncoderConfig:
+    hf_model: str = 'google-bert/bert-base-uncased'
+    pooling: str = 'cls' # 'avg' or 'cls' or 'att'
+
+
 class BaselineDialogueEncoder(nn.Module, HParamsPuller, BaseDialogueModel):
-    def __init__(self, hf_model_name):
+    def __init__(self, config: BaselineDialogueEncoderConfig):
         super().__init__()
 
-        self.hf_model_name = hf_model_name
+        self.config = config
 
-        self.model = AutoModel.from_pretrained(hf_model_name)
-        self.tokenizer = AutoTokenizer.from_pretrained(hf_model_name)
+        self.model = AutoModel.from_pretrained(config.hf_model)
+        self.tokenizer = AutoTokenizer.from_pretrained(config.hf_model)
+
+        if self.config.pooling == 'cls':
+            self.pooler = CLSPooling()
+        elif self.config.pooling == 'avg':
+            self.pooler = AveragePooling()
+        elif self.config.pooling == 'att':
+            self.pooler = SelfAttentionPooling(self.model.config.hidden_size)
+        else:
+            raise ValueError('unknown pooler name')
 
     @property
     def device(self):
@@ -19,19 +38,37 @@ class BaselineDialogueEncoder(nn.Module, HParamsPuller, BaseDialogueModel):
 
     @staticmethod
     def _parse(dia):
+        """
+        return list of strings of the following format: 
+        [
+            "0 Hello, dear!",
+            "1 Hi.",
+            "0 How can I help you?",
+            "1 Tell me a joke."
+        ]
+        """
         return [f'{item["speaker"]} {item["utterance"]}' for item in dia]
 
     @staticmethod
     def _tokenize(tokenizer, batch):
+        """
+        1. convert each dialogue to a single string of the following format: 
+            "0 Hello, dear![SEP]1 Hi.[SEP]0 How can I help you?[SEP]1 Tell me a joke."
+        2. tokenize every string with the standard tokenizer of the provided hf model
+        """
         sep = tokenizer.sep_token
         parsed = [sep.join(BaselineDialogueEncoder._parse(dia)) for dia in batch]
         inputs = tokenizer(parsed, padding='longest', return_tensors='pt')
         return inputs
     
     def forward(self, batch):
+        """
+        input: batch of dialogues
+        output: (B,H)
+        """
         inputs = self._tokenize(self.tokenizer, batch).to(self.device)
-        hidden_states = self.model(**inputs).last_hidden_state      # (B, T, H)
-        encodings = hidden_states[:, 0, :]                          # (B, H)
+        outputs = self.model(**inputs)
+        encodings = self.pooler(outputs.last_hidden_state, inputs.attention_mask)
         return encodings
 
     def get_hidden_size(self):
--- mylib/utils/modeling/poolers.py
+++ mylib/utils/modeling/poolers.py
@@ -1,10 +1,11 @@
+import torch
 from torch import nn
 import torch.nn.functional as F
 
 
 class CLSPooling(nn.Module):
     """just take first token from each sequence in batch"""
-    def forward(self, last_hidden_state, **kwargs):
+    def forward(self, last_hidden_state, attention_mask=None):
         """
         last_hidden_state: (B,T,H)
         return: (B,H)
@@ -13,14 +14,14 @@ class CLSPooling(nn.Module):
 
 
 class AveragePooling(nn.Module):
-    def forward(self, last_hidden_state, attention_mask, **kwargs):
+    def forward(self, last_hidden_state, attention_mask):
         """
         last_hidden_state: (B,T,H)
         attention_mask: (B,T), mask where 0 indicates tokens not to attend
         return: (B,H)
         """
         hidden_states = last_hidden_state * attention_mask[..., None]
-        return hidden_states.sum(dim=1) / attention_mask.sum(dim=1)
+        return hidden_states.sum(dim=1) / attention_mask.sum(dim=1, keepdims=True)
 
 
 class SelfAttentionPooling(nn.Module):
@@ -38,14 +39,15 @@ class SelfAttentionPooling(nn.Module):
         
         self.Q = nn.Linear(input_dim, 1)
         
-    def forward(self, last_hidden_state, **kwargs):
+    def forward(self, last_hidden_state, attention_mask):
         """
         last_hidden_state: (B, T, H)
         return: (B, H)
         """
 
-        att_scores = self.Q(last_hidden_state).squeeze(-1)              # (B,T)
-        att_probs = F.softmax(att_scores)                           # (B,T)
-        res = (last_hidden_state * att_probs[..., None]).sum(dim=1)     # (B,H)
+        att_scores = self.Q(last_hidden_state).squeeze(-1)                      # (B,T)
+        att_scores = att_scores.masked_fill(attention_mask == 0, -torch.inf)    # (B,T)
+        att_probs = F.softmax(att_scores, dim=1)                                # (B,T)
+        res = (last_hidden_state * att_probs[..., None]).sum(dim=1)             # (B,H)
 
         return res
