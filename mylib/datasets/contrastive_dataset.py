e241743012a235e978ee95d97c3da176484797a5
voorhs <ilya_alekseev_2016@list.ru>
2024-03-20 12:11:53 +0300

    add simple augmentations

--- mylib/datasets/contrastive_dataset.py
+++ mylib/datasets/contrastive_dataset.py
@@ -1,6 +1,6 @@
 from torch.utils.data import Dataset
 from datasets import load_from_disk
-from random import choice
+from random import choice, randint
 
 
 class ContrastiveDataset(Dataset):
@@ -8,6 +8,7 @@ class ContrastiveDataset(Dataset):
         self.path = path
 
         self.dataset = load_from_disk(path)
+        self.augs = [precomputed, halves, response_selection, span_matching]
 
     def __len__(self):
         return self.dataset.num_rows
@@ -20,9 +21,52 @@ class ContrastiveDataset(Dataset):
             "pos": one of augmentations' content,
         }
         """
-        sample =  self.dataset[i]
-        orig = sample['orig']['content']
-        
-        positive = choice(sample['pos'])['content']
+        sample = self.dataset[i]
+        aug = choice(self.augs)
+        return aug(sample)
 
-        return orig, positive
+
+def get_split_index(dia):
+    if len(dia) == 2:
+        return 1
+    else:
+        return randint(a=1, b=len(dia)-2)
+
+
+def halves(sample):
+    dia = sample['orig']['content']
+    
+    split_index = get_split_index(dia)
+
+    return dia[:split_index], dia[split_index:]
+
+
+def precomputed(sample):
+    orig = sample['orig']['content']
+    
+    positive = choice(sample['pos'])['content']
+
+    return orig, positive
+
+
+def response_selection(sample):
+    dia = sample['orig']['content']
+
+    split_index = get_split_index(dia)
+
+    return dia[max(0,split_index-3):split_index], [dia[split_index]]
+
+
+def span_matching(sample):
+    dia = sample['orig']['content']
+
+    if len(dia) <= 3:
+        return halves(sample)
+    
+    start = randint(a=1, b=len(dia)-2)
+    end = randint(a=start+1, b=len(dia)-1)
+
+    span = dia[start:end]
+    negate_span = dia[:start] + dia[end:]
+    
+    return span, negate_span
